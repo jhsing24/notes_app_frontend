62cea9ab57549e5ab29bcb1c4931991bef6b87a0
jhsing24 <jerohsing@gmail.com>
2022-08-23 13:49:51 -0700

    Implemented CRU functions to notes and users

--- src/features/auth/Welcome.js
+++ src/features/auth/Welcome.js
@@ -12,8 +12,10 @@ const Welcome = () => {
             <h1>Welcome!</h1>
 
             <p><Link to="/dash/notes">View techNotes</Link></p>
+            <p><Link to="/dash/notes/new">Add new techNote</Link></p>
 
             <p><Link to="/dash/users">View User Settings</Link></p>
+            <p><Link to="/dash/users/new">Add new User </Link></p>
 
         </section>
     )
